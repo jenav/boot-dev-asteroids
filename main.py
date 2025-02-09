82da9cdb1302614847d118e0c14aeb395fb324aa
jenav <jenav@example.com>
2025-02-09 14:03:26 +0000

    implement Groups. change keys handling

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
@@ -22,12 +26,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q]:
+            return
+
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        for u in updatable:
+            u.update(dt, keys)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) * 0.001
 
 
 if __name__ == "__main__":
     main()
+    print("Exiting...")
--- player.py
+++ player.py
@@ -22,9 +22,7 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    def update(self, dt):
-        keys = pygame.key.get_pressed()
-
+    def update(self, dt, keys):
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
