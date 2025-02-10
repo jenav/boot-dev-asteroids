c100577ab23bed28e1aed3a392464cf3040af29d
Jerónimo Navarro <jnavarro@ancasrl.com.ar>
2025-02-10 11:58:50 -0300

    only update score text when needed

--- score.py
+++ score.py
@@ -5,14 +5,16 @@ from constants import *
 class Score:
     def __init__(self):
         self.score = 0
+        self.score_text = f"Score: {self.score}"
         self.font = pygame.font.SysFont(SCORE_FONT, SCORE_SIZE)
 
     def draw(self, screen):
-        text_surface = self.font.render(f"Score: {self.score}", True, SCORE_COLOR)
+        text_surface = self.font.render(self.score_text, True, SCORE_COLOR)
         screen.blit(text_surface, SCORE_POSITION)
 
     def update(self):
         self.score += 20
+        self.score_text = f"Score: {self.score}"
 
     def get_score(self):
         return self.score
